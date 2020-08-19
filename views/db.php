da77e91a36d2b9ff99d2ffa66d18ac122b391a6b
DarkProjectz <darkprojeckz@gmail.com>
2020-08-19 09:45:57 +0530

    Updated db.php

--- views/db.php
+++ views/db.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "us-cdbr-iron-east-01.cleardb.net";
-$username = "b001494fc38d85";
-$password = "246b1c85";
-$dbname = "heroku_5ecc95cd61ddfa7";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "sample";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
